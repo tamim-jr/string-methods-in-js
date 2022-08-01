2e79dabf370eff31d549f5768d466c98d9c44f5f
Tamim Mahmud <internationalinf24@gmail.com>
2022-08-02 00:41:19 +0600

    String method in JavaScript

---
+++ app.js
@@ -0,0 +1,18 @@
+let part1 = "This is the first example of this js tutorial. ";
+let part2 = "This is the second example of this tutorial.";
+
+//CharAt in JavaScript
+console.log(part1.charAt(3));
+
+
+//charCodeAt in JavaScript
+console.log(part1.charCodeAt(2));
+
+//concat
+console.log(part1.concat(part2));
+
+//endsWith
+console.log(part1.endsWith("JavaScript"));
+
+//includes
+console.log(part1.includes("This"));
