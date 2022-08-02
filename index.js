16a0b5a2cdc7e3a82d26513688d9d50d3076c144
Tamim Mahmud <internationalinf24@gmail.com>
2022-08-02 23:27:13 +0600

    set upper case and lower case in js

--- index.js
+++ index.js
@@ -4,7 +4,15 @@
 
 // alert(slicedTweet + "\nYour original tweet size is" + tweet.length);
 
-let firstName = prompt("Type your name");
-let upper = firstName[0].toLocaleUpperCase();
+// let firstName = prompt("Type your name");
+// let upper = firstName[0].toLocaleUpperCase();
 
-alert(upper + firstName.slice(1, 30) + "\nYour original name size is" + firstName.length);
+// alert(upper + firstName.slice(1, 30) + "\nYour original name size is" + firstName.length);
+
+let userName = prompt("Type your name");
+let toLower = userName.toLocaleLowerCase();
+let toUpper = userName.toLocaleUpperCase();
+
+let lastPart = toLower.slice(1);
+let firstPart = toUpper.slice(0, 1);
+alert(firstPart + lastPart);
