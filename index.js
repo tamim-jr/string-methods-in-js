50726e7938f6fdf6d6e7d2340d5a51348b9aa2ab
Tamim Mahmud <internationalinf24@gmail.com>
2022-08-02 22:08:02 +0600

    auto capitalize app

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-let tweet = prompt("Type your tweet in here");
+// let tweet = prompt("Type your tweet in here");
 
-let slicedTweet = tweet.slice(0, 150);
+// let slicedTweet = tweet.slice(0, 150);
 
-alert(slicedTweet + "\nYour original tweet size is" + tweet.length);
+// alert(slicedTweet + "\nYour original tweet size is" + tweet.length);
+
+let firstName = prompt("Type your name");
+let upper = firstName[0].toLocaleUpperCase();
+
+alert(upper + firstName.slice(1, 30) + "\nYour original name size is" + firstName.length);
